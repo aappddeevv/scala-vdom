7af17dcc4f91d7c0143b2f2eb21f62b062bef5f7
Joe Barnes <barnesjd@gmail.com>
2016-04-13 14:00:57 -0500

    Implemented basic scala.xml.Node => VNode conversion

--- jvm/src/test/scala/im/vdom/ScalaXmlSpec.scala
+++ jvm/src/test/scala/im/vdom/ScalaXmlSpec.scala
@@ -2,13 +2,49 @@ package im.vdom
 
 import org.scalatest.WordSpec
 import ScalaXml._
-
-import scala.xml.Node
+import VNode._
 
 class ScalaXmlSpec extends WordSpec {
-  "ScalaXml.nodeSeqToVNode()" should {
-    "convert an empty NodeSeq into VNode.empty" in {
-      assertResult(VNode.empty)(nodeToVNode(<empty/>))
+  "ScalaXml.nodeToVNode()" should {
+    "convert a trivial empty Node into a VNode.tag" in {
+      val html = <br/>
+      val expected = tag("br")
+
+      assertResult(expected)(nodeToVNode(html))
+    }
+
+    "convert an empty Node with attributes" in {
+      val html = <div id="my-id" class="my-class"></div>
+      val expected = tag("div", Seq(KeyValue(AttrKey("id"), Some("my-id")), KeyValue(AttrKey("class"), Some("my-class"))))
+
+      assertResult(expected)(nodeToVNode(html))
+    }
+
+    "convert a Node with text" in {
+      val html = <span>This is text</span>
+      val expected = tag("span", Seq(), text("This is text"))
+
+      assertResult(expected)(nodeToVNode(html))
+    }
+
+    "convert a Node with children and attributes" in {
+      val html =
+        <form method="post">
+          <div>
+            <input type="text" id="chat-in" name="in"/>
+            <input type="submit" value="Submit"/>
+          </div>
+        </form>
+
+      val expected =
+        tag("form", Seq(KeyValue(AttrKey("method"), Some("post"))),
+          tag("div", Seq(),
+            tag("input", Seq(KeyValue(AttrKey("type"), Some("text")), KeyValue(AttrKey("id"), Some("chat-in")), KeyValue(AttrKey("name"), Some("in")))),
+            tag("input", Seq(KeyValue(AttrKey("type"), Some("submit")), KeyValue(AttrKey("value"), Some("Submit"))))
+          )
+        )
+
+      assertResult(expected)(nodeToVNode(html))
     }
   }
 
